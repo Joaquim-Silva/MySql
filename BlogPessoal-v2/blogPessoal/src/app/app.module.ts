870eb0572e407983429c719fb642e209c0306c82
Joaquim Silva <joaquim.diego8@gmail.com>
2021-01-26 20:55:58 -0300

    componente tema design

--- BlogPessoal-v2/blogPessoal/src/app/app.module.ts
+++ BlogPessoal-v2/blogPessoal/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 
@@ -23,7 +24,8 @@ import { InicioComponent } from './inicio/inicio.component';
     EntrarComponent,
     CadastrarComponent,
     MenuComponent,
-    InicioComponent
+    InicioComponent,
+    TemaComponent
 
 
   ],
